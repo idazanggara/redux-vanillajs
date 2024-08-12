03c19229244bb2841fdb62881aa31870c3ce2853
idazanggara <idazideas@gmail.com>
2024-08-12 13:35:47 +0700

    second commit multi file

--- app.js
+++ app.js
@@ -6,64 +6,92 @@
 - Middleware -> Layer atau fungsi tambahan di antara dispatching action
 */
 
-import { createSlice, configureStore } from '@reduxjs/toolkit'
+// import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-// slice di buat masing-masing sesuai kebutuhan kalian
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: {
-    number: 0,
+// // slice di buat masing-masing sesuai kebutuhan kalian
+// const counterSlice = createSlice({
+//   name: 'counter',
+//   initialState: {
+//     number: 0,
 
-    // nah kalau begini nanti ribet ya, apalagi buat reducersnya, nanti ribet bedainyanya. mending di buat slice masing-masing aja. per fitur yg di inginkan
-    todos: [],
-    products: [],
-    customers: [],
-    admin: [],
-  },
-  reducers: {
-    // counter/increment
-    increment: (state, action) => {
-      const { type, payload } = action
-      state.number += action.payload
-      // state.number = state.number + 1
-    },
-    // counter/decrement
-    decrement: (state, payload) => {
-      // state.number = payload
-      state.number = state.number - 1
-    }
-  }
-})
+//     // nah kalau begini nanti ribet ya, apalagi buat reducersnya, nanti ribet bedainyanya. mending di buat slice masing-masing aja. per fitur yg di inginkan
+//     todos: [],
+//     products: [],
+//     customers: [],
+//     admin: [],
+//   },
+//   reducers: {
+//     // counter/increment
+//     increment: (state, action) => {
+//       const { type, payload } = action
+//       state.number += action.payload
+//       // state.number = state.number + 1
+//     },
+//     // counter/decrement
+//     decrement: (state, payload) => {
+//       // state.number = payload
+//       state.number = state.number - 1
+//     }
+//   }
+// })
 
-// ini yg di export nanti
-// ini cara gampangnya sekarang, kita ambil dari slicenya, sudah jadi action disini
-export const { increment, decrement } = counterSlice.actions
+// // ini yg di export nanti
+// // ini cara gampangnya sekarang, kita ambil dari slicenya, sudah jadi action disini
+// export const { increment, decrement } = counterSlice.actions
 
-const store = configureStore({
-  reducer: {
-    counter: counterSlice.reducer
-  }
-})
+// const store = configureStore({
+//   reducer: {
+//     counter: counterSlice.reducer
+//   }
+// })
 
-// ngecek setiap ada yg melakukan dispatch
+// // ngecek setiap ada yg melakukan dispatch
+// store.subscribe(() => {
+//   console.log("🚀 ~ store.subscribe ~ store.getState:", store.getState())
+// })
+
+// // store.dispatch(increment())
+// // store.dispatch(increment())
+// // store.dispatch(increment())
+// // store.dispatch(increment())
+
+// // ini cara dulu untuk membuat action, kalau sekarang kan action sudah di buatkan
+// store.dispatch({
+//   type: 'counter/increment',
+//   payload: 2
+// })
+// store.dispatch({
+//   type: 'counter/increment',
+//   payload: 2
+// })
+// console.log("🚀 ~ increment():", increment(100))
+// store.dispatch(increment(10))
+
+
+// kita comment diatas, kita coba case lain yg mendekati reactjs nanti
+
+// kita panggil redux toolkitnya
+import store from "./store.js"
+import { TodoService } from "./todoService.js"
+
 store.subscribe(() => {
-  console.log("🚀 ~ store.subscribe ~ store.getState:", store.getState())
+  console.log("🚀 ~ store.subscribe ~ store.getState:", store.getState().todo)
 })
 
-// store.dispatch(increment())
-// store.dispatch(increment())
-// store.dispatch(increment())
-// store.dispatch(increment())
 
-// ini cara dulu untuk membuat action, kalau sekarang kan action sudah di buatkan
-store.dispatch({
-  type: 'counter/increment',
-  payload: 2
-})
-store.dispatch({
-  type: 'counter/increment',
-  payload: 2
-})
-console.log("🚀 ~ increment():", increment(100))
-store.dispatch(increment(10))
+const todoService = new TodoService()
 
+// console.log("🚀 ~ todoService.getAllTodo():", todoService.getAllTodo())
+
+todoService.createTodo({
+  id: 1,
+  title: 'Mokel',
+  description: 'adnan batal puasa, astagfirullah',
+  status: false
+
+})
+
+todoService.getById(1)
+// console.log("🚀 ~ todoService.getById(1):", todoService.getById(1))
+
+todoService.getAllTodo() // seperti ini kenapa tidak ada respon apapun. karena tidak ada perubahan yg terjadi. jadi subcribenya tidak mengirim respon
